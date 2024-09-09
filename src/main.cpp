c854933b9ced272ac5ae01a5995949541f514fff
df1gg <samsung26032002@gmial.com>
2024-09-10 02:37:24 +0300

    first start cpu

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
+#include "CPU.h"
 #include <iostream>
+#include <memory.h>
 
 
 int main()
 {
+    CPU cpu;
+    Memory memory;
+
+    memory.write(0x0000, 0xA9); // LDA
+    memory.write(0x0007, 0x00); // BRK
+
     std::cout << "CPU started..." << "\n";
+    cpu.execute();
+    std::cout << "CPU finish work..." << "\n";
+
     return 0;
 }
